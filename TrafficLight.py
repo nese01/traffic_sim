1d4004993fc07f614025a8ec76a8bdb6c01e7e98
Nese <enesozcan235@gmail.com>
2025-12-03 16:54:16 -0800

    changed it to separate the individual lights and the set of lights by putting it into different classes

--- TrafficLight.py
+++ TrafficLight.py
@@ -8,27 +8,40 @@ Traffic light is an object that should be placed right before an intersection
 Three states: "RED", "YELLOW", and "GREEN"
 """
 
-class TrafficLight:
-    def __init__(self, x_pos, y_pos, weight=30, counterweight=30, init_state="RED", yellow_timer=2):
-        self.xpos = x_pos
-        self.ypos = y_pos
-        self.timer = weight
-        self.red_timer = counterweight
-        self.state = init_state
-        if init_state == "RED": self.current_time = counterweight
-        else: self.current_time = weight
-        self.yellow_timer = yellow_timer
+class TrafficLightSet:
+    def __init__(self, positions, y_green_time=30, x_green_time=30, lanes=1, detection="time_cycle"):
+        #get all the maximum and minimum locations to determine where the traffic lights will be stored
+        self.positions = positions
+        self.north = []
+        self.south = []
+        self.west = []
+        self.east = []
+        for i in range(0, 4*lanes, 4):
+            self.south.append(TrafficLight(positions[i], state="GREEN"))
+            self.north.append(TrafficLight(positions[i+1], state="GREEN"))
+            self.west.append(TrafficLight(positions[i+2], state="RED"))
+            self.east.append(TrafficLight(positions[i+3], state="RED"))
+
+        #decides whether y-axis is green or x-axis is green
+        self.y_turn = True
+
+        self.y_timer = y_green_time
+        self.x_timer = x_green_time
+        self.detection = detection
+
+        self.yellow_timer = lanes*2
         self.priority = 0 #dummy code, will implement fully later
 
-    """
-    Step function, default mode
-    Will decrement the timer it has on itself and switch states automatically
-    Perfect for global independent function
-    For advanced simulations, requires location and nearby traffic analysis with detect_car
-    """
-    def step(self, type=None):
-        match type:
-            case None: return "Error, please enter a type"
+
+    def step(self):
+        """
+        Step function, default mode
+        Will decrement the timer it has on itself and switch states automatically
+        Perfect for global independent function
+        For advanced simulations, requires location and nearby traffic analysis with detect_car
+        """
+        match self.detection:
+            case None: return "Error, no detection type"
 
             case "time_cycle":
                 self.current_time = self.current_time - 1
@@ -45,24 +58,38 @@ class TrafficLight:
                     if self.current_time < 0:
                         self.state = "RED"
                         self.current_time = self.red_timer
+
+
             case "detection cycle":
                 return #implement later
 
+            case _:
+                return "Error, detection type invalid"
 
 
 
-    """
-    Placeholder for potential additional mode
-    Function to detect car, a dummy version for now that will try and search for nearby traffic lights in the
-    Environment class for more accurate simulations.
-    This makes more sense to do in environment class, might delete later
-    """
+
+
     def detect_car(self, environment_grid):
+        """
+        Placeholder for potential additional mode
+        Function to detect car, a dummy version for now that will try and search for nearby traffic lights in the
+        Environment class for more accurate simulations.
+        This makes more sense to do in environment class, might delete later
+        """
         if environment_grid(self.get_pos()).has_car(): return False
 
     """
     Get Position
     Returns the position in numpy tuple format to make numpy calculation regarding position easier
     """
+
+class TrafficLight():
+    def __init__(self, position, state="RED"):
+        self.position = position
+        self.state = state
+
+
+
     def get_pos(self):
         return self.ypos, self.xpos
